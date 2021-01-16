9796423a58664c5d17db2d167e78d9986f14b7ff
SteveDexter24 <tamangsteve@gmail.com>
2021-01-17 03:09:49 +0800

    Remove duplicated directory

---
+++ streams/client/src/components/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import  { BrowserRouter, Route, Link } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+
+
+
+const App = () => {
+    return (
+        <div>
+            <h1>Header</h1>
+            <BrowserRouter>
+                <div>
+                    <Route path='/' exact component={StreamList} />
+                    <Route path='/streams/new' exact component={StreamCreate} />
+                    <Route path='/streams/edit' exact component={StreamEdit} />
+                    <Route path='/streams/delete' exact component={StreamDelete} />
+                    <Route path='/streams/show' exact component={StreamShow} />
+                </div>
+            </BrowserRouter>
+        </div>
+    );
+}
+
+
+export default App;
