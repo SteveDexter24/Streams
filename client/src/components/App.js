2163334a614a467d752f45724fd0fb7c0f51815f
SteveDexter24 <tamangsteve@gmail.com>
2021-01-18 00:28:30 +0800

    Redux form added

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -9,7 +9,7 @@ import Header from './Header';
 
 const App = () => {
 	return (
-		<div>
+		<div className="ui container">
 			<BrowserRouter>
 				<Header />
 				<div>
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux';
+// re-name to formReducer using the as keyword
+import { reducer as formReducer } from 'redux-form';
 import authReducer from './authReducer';
 
 export default combineReducers({
-    auth: authReducer
+	auth : authReducer,
+	form : formReducer
 });
