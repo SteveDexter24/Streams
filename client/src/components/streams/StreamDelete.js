f0a1c466c90d1080b9e2fa9eb89d27f6917e82cf
SteveDexter24 <tamangsteve@gmail.com>
2021-01-24 01:18:38 +0800

    delete stream done

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,28 +1,54 @@
 import React from "react";
 import Modal from "../Modal";
 import history from "../../history";
+import { deleteStreams, fetchStream } from "../../actions";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 // React.Fragment or empty tags <></>: something like an invisible element that doesn't have any impact on the DOM
 
-const StreamDelete = () => {
-    const actions = (
-        <React.Fragment>
-            <button className="ui button negative">Delete</button>
-            <button className="ui button">Cancel</button>
-        </React.Fragment>
-    );
+class StreamDelete extends React.Component {
+    componentDidMount() {
+        // get the id variable from the path using props.match.params.id
+        this.props.fetchStream(this.props.match.params.id);
+    }
 
-    return (
-        <div>
-            StreamDelete
+    renderActions() {
+        const { id } = this.props.match.params;
+        return (
+            <React.Fragment>
+                <button onClick={() => this.props.deleteStreams(id)} className="ui button negative">
+                    Delete
+                </button>
+                <Link to="/" className="ui button">
+                    Cancel
+                </Link>
+            </React.Fragment>
+        );
+    }
+
+    renderContent() {
+        if (!this.props.stream) {
+            return "Are you sure you want to delete this stream?";
+        }
+
+        return `Are you sure you want to delete this stream with title: ${this.props.stream.title}`;
+    }
+
+    render() {
+        return (
             <Modal
                 title="Delete Stream"
-                content="Are you sure you want to delete this stream"
-                actions={actions}
+                content={this.renderContent()}
+                actions={this.renderActions()}
                 onDismiss={() => history.push("/")}
             />
-        </div>
-    );
+        );
+    }
+}
+
+const mapStateToProps = (state, ownProps) => {
+    return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default StreamDelete;
+export default connect(mapStateToProps, { deleteStreams, fetchStream })(StreamDelete);
--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,75 +1,75 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { fetchStreams } from '../../actions';
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { fetchStreams } from "../../actions";
 
 class StreamList extends React.Component {
-	componentDidMount() {
-		this.props.fetchStreams();
-	}
+    componentDidMount() {
+        this.props.fetchStreams();
+    }
 
-	// edit or delete
-	renderAdmin(stream) {
-		if (stream.userId === this.props.currentUserId) {
-			return (
-				<div className="right floated content">
-					<Link to={`/streams/edit/${stream.id}`} className="ui button primary">
-						Edit
-					</Link>
-					<button className="ui button negative">
-						{/*see semantic ui docs for negative: red*/}
-						Delete
-					</button>
-				</div>
-			);
-		}
-	}
+    // edit or delete
+    renderAdmin(stream) {
+        if (stream.userId === this.props.currentUserId) {
+            return (
+                <div className="right floated content">
+                    <Link to={`/streams/edit/${stream.id}`} className="ui button primary">
+                        Edit
+                    </Link>
+                    <Link to={`/streams/delete/${stream.id}`} className="ui button negative">
+                        {/*see semantic ui docs, negative: red*/}
+                        Delete
+                    </Link>
+                </div>
+            );
+        }
+    }
 
-	renderList() {
-		return this.props.streams.map((stream) => {
-			return (
-				<div className="item" key={stream.id}>
-					{this.renderAdmin(stream)}
-					<i className="large middle aligned icon camera" />
-					<div className="content">
-						{stream.title}
-						<div className="description">{stream.description}</div>
-					</div>
-				</div>
-			);
-		});
-	}
+    renderList() {
+        return this.props.streams.map((stream) => {
+            return (
+                <div className="item" key={stream.id}>
+                    {this.renderAdmin(stream)}
+                    <i className="large middle aligned icon camera" />
+                    <div className="content">
+                        {stream.title}
+                        <div className="description">{stream.description}</div>
+                    </div>
+                </div>
+            );
+        });
+    }
 
-	renderCreate() {
-		if (this.props.isSignedIn) {
-			return (
-				<div style={{ textAlign: 'right' }}>
-					<Link to="/streams/new" className="ui button primary">
-						Create Stream
-					</Link>
-				</div>
-			);
-		}
-	}
+    renderCreate() {
+        if (this.props.isSignedIn) {
+            return (
+                <div style={{ textAlign: "right" }}>
+                    <Link to="/streams/new" className="ui button primary">
+                        Create Stream
+                    </Link>
+                </div>
+            );
+        }
+    }
 
-	render() {
-		return (
-			<div>
-				<h2>Streams</h2>
-				<div className="ui celled list">{this.renderList()}</div>
-				{this.renderCreate()}
-			</div>
-		);
-	}
+    render() {
+        return (
+            <div>
+                <h2>Streams</h2>
+                <div className="ui celled list">{this.renderList()}</div>
+                {this.renderCreate()}
+            </div>
+        );
+    }
 }
 
 const mapStateToProps = (state) => {
-	// Object.values() turns values in an array of objects to array of values from the object
-	return {
-		streams: Object.values(state.streams),
-		currentUserId: state.auth.userId,
-		isSignedIn: state.auth.isSignedIn
-	};
+    // Object.values() turns values in an array of objects to array of values from the object
+    return {
+        streams: Object.values(state.streams),
+        currentUserId: state.auth.userId,
+        isSignedIn: state.auth.isSignedIn,
+    };
 };
 
 export default connect(mapStateToProps, { fetchStreams })(StreamList);
